094e9adb6346d8d270386f8755558c2f453c1d30
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-25 21:29:32 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
--- Task 3: List Glam rock bands ranked by longevity
-SELECT band_name, 
-       IF(split = 0, 2022 - formed, split - formed) AS lifespan
-FROM bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC, band_name ASC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
