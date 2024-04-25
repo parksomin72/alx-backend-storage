9d9747cfdef1f3d254d54864b220d2974cb9994e
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-25 21:47:57 +0000

    Average weighted score for all!

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,23 @@
--- Task 7: Create stored procedure ComputeAverageScoreForUser
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+-- Creates a stored procedure ComputeAverageScoreForUser that
+-- computes and stores the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-    
-    -- Compute average score for the user
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-    
-    -- Update the average_score in the users table
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-END;
-//
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
+
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,3 @@
--- Task 9: Create an index on the first letter of name and score
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+-- Creates an index idx_name_first_score on the table names and
+-- the first letter of name and the score.
+CREATE INDEX idx_name_first_score ON names(name(1), score);
