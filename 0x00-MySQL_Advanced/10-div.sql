367d1f8b3d78b602e25f9d9220d3e77f859611a4
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-25 21:44:21 +0000

    Average weighted score for all!

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,18 +1,15 @@
--- Task 10: Create a function SafeDiv
-DELIMITER //
-
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10,4)
+-- creates a function SafeDiv that divides (and returns) the first
+-- by the second number or returns 0 if the second number is equal to 0.
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    DECLARE result DECIMAL(10,4);
-    
-    IF b = 0 THEN
-        SET result = 0;
-    ELSE
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
         SET result = a / b;
     END IF;
-    
     RETURN result;
-END //
-
+END $$
 DELIMITER ;
