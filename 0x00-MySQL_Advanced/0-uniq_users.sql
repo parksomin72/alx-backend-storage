320840939108c0f0103679483eebab4606b80b30
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-25 21:16:57 +0000

    We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Task 0: Create a table users with unique email constraint
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
