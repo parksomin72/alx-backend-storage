59469609c8d1057071c7cbd3fd9cc7e5c199ab49
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-25 21:24:25 +0000

    Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- Task 4: Create trigger to decrease item quantity after adding an order
+DELIMITER //
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER ;
