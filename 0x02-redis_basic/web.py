a1062ac8e23055cad976a7d4ffa92d38496b1b23
parksomin72 <fatimazahraealawi@gmail.com>
2024-05-01 16:04:40 -0700

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python3
-"""
-Web cache and tracker using Redis
-"""
-import requests
+""" expiring web cache module """
+
 import redis
-import time
+import requests
+from typing import Callable
+from functools import wraps
 
-# Initialize Redis client
-redis_client = redis.Redis()
+redis = redis.Redis()
 
+
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
+
+
+@wrap_requests
 def get_page(url: str) -> str:
-    # Track URL access count
-    count_key = f"count:{url}"
-    redis_client.incr(count_key)
-
-    # Check if HTML content is cached
-    cached_content = redis_client.get(url)
-    if cached_content:
-        return cached_content.decode('utf-8')
-
-    # Fetch HTML content from the URL
+    """get page self descriptive
+    """
     response = requests.get(url)
-    html_content = response.text
-
-    # Cache HTML content with expiration time of 10 seconds
-    redis_client.setex(url, 10, html_content)
-
-    return html_content
-
-if __name__ == "__main__":
-    # Example usage
-    url = "http://slowwly.robertomurray.co.uk"
-    html = get_page(url)
-    print(html)
+    return response.text
